f11820696d2d7e253778e690beeda808fe321886
Ramon Tayag <ramon.tayag@gmail.com>
2018-12-12 13:58:29 +0800

    Puts for debugging

--- lib/stellar_spectrum/services/get_channel_accounts.rb
+++ lib/stellar_spectrum/services/get_channel_accounts.rb
@@ -6,6 +6,7 @@ module StellarSpectrum
     promises :channel_accounts
 
     executed do |c|
+      puts "Making accounts from #{c.seeds.inspect}"
       c.channel_accounts = c.seeds.map do |seed|
         Stellar::Account.from_seed(seed)
       end
