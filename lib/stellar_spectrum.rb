75bcc0f8737d8ffa3295ded1654c072444e8fc8f
Ramon Tayag <ramon.tayag@gmail.com>
2018-12-13 20:29:43 +0800

    Check if the sequence number in horizon has bumped up before unlocking
    Do not just unlock, it's possible that the sequence on horizon's hasn't
    bumped up yet. Keep trying to unlock until it is able to unlock.

--- lib/stellar_spectrum.rb
+++ lib/stellar_spectrum.rb
@@ -8,6 +8,7 @@ require "active_support/core_ext/hash/except"
 require "active_support/core_ext/object/blank"
 require 'active_support/core_ext/integer/inflections'
 require "stellar_spectrum/services/get_available_channels"
+require "stellar_spectrum/services/get_channel_account_info"
 require "stellar_spectrum/services/get_channel_accounts"
 require "stellar_spectrum/services/get_key_for_address"
 require "stellar_spectrum/services/get_locked_accounts"
@@ -18,9 +19,12 @@ require "stellar_spectrum/services/init_stellar_client"
 require "stellar_spectrum/services/lock_channel"
 require "stellar_spectrum/services/pick_channel"
 require "stellar_spectrum/services/send_payment"
+require "stellar_spectrum/services/sending_payment/attempt_release_lock"
 require "stellar_spectrum/services/sending_payment/retry"
 require "stellar_spectrum/services/sending_payment/send_asset"
-require "stellar_spectrum/services/unlock"
+require "stellar_spectrum/services/unlocking/attempt_release"
+require "stellar_spectrum/services/unlocking/check_sequence_number"
+require "stellar_spectrum/services/unlocking/unlock"
 
 module StellarSpectrum
 
