21ca9d0fdd66da58c4a1ba8cf5a68e1b73657046
Mark Chavez <markchav3z@gmail.com>
2019-01-18 01:35:54 +0800

    Retry `SendingPayments::SendAsset` when it fails

--- spec/lib/stellar_spectrum/services/sending_payment/send_asset_spec.rb
+++ spec/lib/stellar_spectrum/services/sending_payment/send_asset_spec.rb
@@ -124,7 +124,7 @@ module StellarSpectrum
         end
       end
 
-      context "other unsuccessful response" do
+      context "a non Faraday::ClientError" do
         let(:from) { Stellar::Account.from_seed(CONFIG[:sender_seed]) }
         let(:to) { Stellar::Account.from_address(CONFIG[:destination_address]) }
         let(:amount) { Stellar::Amount.new(1) }
@@ -137,7 +137,7 @@ module StellarSpectrum
           end
         end
 
-        it "returns the unsuccessful response", vcr: {record: :once} do
+        it "raises the error" do
           expect(stellar_client).to receive(:send_payment).with(
             from: from,
             to: to,
@@ -145,7 +145,7 @@ module StellarSpectrum
             memo: nil,
             transaction_source: channel_account,
             sequence: 1,
-          ).and_call_original
+          ).and_raise(StandardError)
 
           expect do
             described_class.execute(
@@ -161,43 +161,55 @@ module StellarSpectrum
               horizon_url: CONFIG[:horizon_url],
               redis_url: CONFIG[:redis_url],
             )
-          end.to raise_error(Faraday::ClientError)
+          end.to raise_error(StandardError)
         end
       end
 
-      context "other Faraday::ClientError without a response method" do
-        let(:from) { Stellar::Account.from_seed(CONFIG[:sender_seed]) }
-        let(:to) { Stellar::Account.from_address(CONFIG[:destination_address]) }
+      context "any Faraday::ClientError is raised" do
+        let(:from) { Stellar::Account.random }
+        let(:to) { Stellar::Account.random }
         let(:amount) { Stellar::Amount.new(1) }
-        let(:channel_account) do
-          Stellar::Account.from_seed(CONFIG[:payment_channel_seeds].sample)
-        end
-        let(:seeds) do
-          CONFIG[:payment_channel_seeds].map do |seed|
-            Stellar::Account.from_seed(seed)
-          end
-        end
+        let(:channel_account) { Stellar::Account.random }
+        let(:seeds) { CONFIG[:payment_channel_seeds] }
+        let(:horizon_url) { CONFIG[:horizon_url] }
+        let(:redis_url) { CONFIG[:redis_url] }
         let(:error) { Faraday::ClientError.new(nil, nil) }
+        let(:retry_result) do
+          LightService::Context.new(send_asset_response: double)
+        end
 
         it "returns the unsuccessful response" do
           expect(stellar_client).to receive(:send_payment)
             .and_raise(error)
 
-          expect do
-            described_class.execute(
-              stellar_client: stellar_client,
-              from: from,
-              to: to,
-              amount: amount,
-              memo: nil,
-              channel_account: channel_account,
-              next_sequence_number: 1,
-              tries: 2,
-              seeds: seeds,
-              horizon_url: CONFIG[:horizon_url],
-              redis_url: CONFIG[:redis_url],
-            )
-          end.to raise_error(error)
+          expect(Retry).to receive(:execute).with(
+            stellar_client: stellar_client,
+            from: from,
+            to: to,
+            amount: amount,
+            memo: nil,
+            tries: 2,
+            seeds: seeds,
+            horizon_url: horizon_url,
+            redis_url: redis_url,
+            force_transaction_source: channel_account,
+            force_sequence_number: 1,
+            force_lock: true,
+          ).and_return(retry_result)
+
+          described_class.execute(
+            stellar_client: stellar_client,
+            from: from,
+            to: to,
+            amount: amount,
+            memo: nil,
+            channel_account: channel_account,
+            next_sequence_number: 1,
+            tries: 2,
+            seeds: seeds,
+            horizon_url: horizon_url,
+            redis_url: redis_url,
+          )
         end
       end
 
