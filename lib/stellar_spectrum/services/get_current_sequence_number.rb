a83f256ac8e4ba22c0706a3bb929179650394398
Mark Chavez <markchav3z@gmail.com>
2019-01-18 01:31:04 +0800

    Retry `GetSequenceNumber` if it fails

---
+++ lib/stellar_spectrum/services/get_current_sequence_number.rb
@@ -0,0 +1,12 @@
+module StellarSpectrum
+  class GetCurrentSequenceNumber
+
+    def self.execute(stellar_client:, channel_account:)
+      account_info = stellar_client.account_info(channel_account)
+      account_info.sequence.to_i
+    rescue Faraday::ClientError => e
+      execute(stellar_client: stellar_client, channel_account: channel_account)
+    end
+
+  end
+end
