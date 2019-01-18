876342c446023cbd67b63ed18cd1bbc74fda8a03
Mark Chavez <markchav3z@gmail.com>
2019-01-18 11:44:42 +0800

    Log retry attempts

--- lib/stellar_spectrum/services/get_current_sequence_number.rb
+++ lib/stellar_spectrum/services/get_current_sequence_number.rb
@@ -5,6 +5,7 @@ module StellarSpectrum
       account_info = stellar_client.account_info(channel_account)
       account_info.sequence.to_i
     rescue Faraday::ClientError => e
+      Log.write("#{Client::LOG_TAG}: Retrying GetCurrentSequenceNumber - #{e.inspect}")
       execute(stellar_client: stellar_client, channel_account: channel_account)
     end
 
